54a503ddef2291a1f46697f4e5a3cc6b43fc982f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2026-04-23 07:23:25 +0600

    Updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://xecom-three.vercel.app'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
